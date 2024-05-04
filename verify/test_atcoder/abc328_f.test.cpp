59f88afac713a7f11e4b91bd1a60e3ac82580b3e
AiletS <stblackeric@gmail.com>
2024-05-05 03:37:43 +0900

    ss

---
+++ verify/test_atcoder/abc328_f.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://atcoder.jp/contests/abc328/tasks/abc328_f"
+#include "my_template.hpp"
+#include "data_structure/Weighted_UnionFind.hpp"
+
+void solve()
+{
+    ll n, q; cin >> n >> q;
+    Weighted_UnionFind<ll> r(n);
+    rep(i, 0, q)
+    {
+        ll a, b, d; cin >> a >> b >> d; a--, b--;
+        if(r.unite(a, b, d)) cout << i + 1 << " ";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+}
---
+++ verify/test_atcoder/abc351_f.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://atcoder.jp/contests/abc351/tasks/abc351_f"
+#include "my_template.hpp"
+#include "data_structure/merge_sort_tree.hpp"
+
+void solve()
+{
+    ll n; cin >> n;
+    vl v(n); cin >> v;
+    merge_sort_tree st(v);
+    ll sum = accumulate(v.begin(), v.end(), 0LL);
+    ll ans = 0;
+    rep(i, 0, n)
+    {
+        sum -= v[i];
+        auto [x, y] = st.get(i + 1, n, v[i]);
+        ans += (sum - y) - v[i] * (n - 1 - i - x);
+    }
+    cout << ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+}
